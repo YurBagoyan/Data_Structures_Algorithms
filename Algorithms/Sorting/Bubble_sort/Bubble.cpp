c13f0c69cac85c733ae7d9cf911b32c9f2fd6aec
Yura Bagoyan <90376899+YurBagoyan@users.noreply.github.com>
2023-01-13 23:44:56 +0400

    Update Bubble.cpp

--- Algorithms/Sorting/Bubble_sort/Bubble.cpp
+++ Algorithms/Sorting/Bubble_sort/Bubble.cpp
@@ -1,55 +1,49 @@
 #include <iostream>
 
-void showArr(int* arr, int const size)
+void printArr(int* arr, int const size)
 {
-    for(int i = 0; i < size; ++i)
-    {
+    for (int i = 0; i < size; ++i) {
         std::cout << arr[i] << " ";
     }
     std::cout << "\n";
 }
 
-int input(int* arr, int const MAXSIZE, int size)
+void inputSize(int& size)
 {
-    std::cout << "Input array size in range [1, 1000]:" << std::endl;
     do {
+        std::cout << "Input array size: ";
         std::cin >> size;
-                    
-    } while (size < 1 || size > MAXSIZE);
 
-        std::cout << "Input array elements of size " << size << ':' << std::endl;
+    } while (size < 1);
+}
+
+void input(int* arr, const int size)
+{
+    std::cout << "Input array elements of size " << size << ':' << std::endl;
     for (int i = 0; i < size; ++i) {
         std::cin >> arr[i];
     }
-
-    return size;
 }
 
-/* Inputs: arr : [integer], size: integer
- * Pre: size < MAXSIZE, size > 1
- * Outputs: arr : [integer]
- * Post: arr[0] <= arr[1] <= ... <= arr[size - 1];
- */
 void bubbleSort(int* arr, int const size)
 {
-    for(int i = size - 1; i > 0; --i) {
-        for(int j = 0; j < i; ++j) {
-            if(arr[j] > arr[j+1]) {
-                std::swap(arr[j], arr[j+1]);
+    for (int i = size - 1; i > 0; --i) {
+        for (int j = 0; j < i; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
             }
         }
     }
 }
 
-int main() 
-{
-    int MAXSIZE = 1000;
-    int arr[MAXSIZE];
-    int size = input(arr, MAXSIZE, size);
+int main()
+{    
+    int size;
+    inputSize(size);
+        
+    int* arr = new int[size];
+    input(arr, size);
 
     bubbleSort(arr, size);
-    showArr(arr, size);
+    printArr(arr, size);
 }
-
-
-//Invariant -> At the end of the iteration of the inner loop, the last element of the array is the largest
