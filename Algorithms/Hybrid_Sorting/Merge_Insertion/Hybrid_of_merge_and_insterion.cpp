26e4cea0084fd940459ba1b02130ac933fe4809f
Yura Bagoyan <90376899+YurBagoyan@users.noreply.github.com>
2023-01-16 23:18:18 +0400

    Update Hybrid_of_merge_and_insterion.cpp

--- Algorithms/Hybrid_Sorting/Merge_Insertion/Hybrid_of_merge_and_insterion.cpp
+++ Algorithms/Hybrid_Sorting/Merge_Insertion/Hybrid_of_merge_and_insterion.cpp
@@ -1,135 +1,115 @@
 #include <iostream>
 
-int input(int* arr, int* size);
-void insertionSort(int* arr, int const start, int const end);
-void merge(int* arr, const int start, const int middle, const int end);
-void mergeSort(int* arr, const int start, const int end, int const k);
-void showArr(int* arr, int const size);
-void sort(int* arr, const int size, int const k);
-
-constexpr int maxSize = 1000;
-
-int main()
+void inputSize(int& size)
 {
-    int arr[maxSize] = { 6, 13, 4, 9, 11, 5, 12, 10, 2, 14, 4, 5, 20, 0, 1  };
-    int const size = 15;
-    //int k = input(arr, &size);
-    
-    int const k = 4;
-    sort(arr, size, k);
+    do {
+        std::cout << "Input array size: ";
+        std::cin >> size;
 
-    std::cout << "Sorted array:" << std::endl;
-    showArr(arr, size);            
+    } while (size < 1);
 }
 
-int input(int* arr, int* size)
-{
-    std::cout << "Input array size in range [1, 1000]:" << std::endl;
-    do {
-        std::cin >> *size;
-    } while (*size < 1 || *size > maxSize);
-    
-    std::cout << "Input array elements of size " << *size << ':' << std::endl;
-    for (int i = 0; i < *size; ++i) {
-        std::cin >> arr[i];    
-    }
-    
-    int k;
-    do {
-        std::cout << "Imp k = ";
-        std::cin >> k;
-    } while (k > *size);
-
-    return k;    
-}
-
-void showArr(int* arr, int const size)
+void input(int* arr, int const size)
 {
     for (int i = 0; i < size; ++i) {
-        std::cout << arr[i] << ' ';                
+        std::cout << "arr[" << i << "] = ";
+        std::cin >> arr[i];
     }
-    std::cout << std::endl;   
 }
 
-void sort(int* arr, const int size, int const k)
+void printArr(int* arr, const int size) 
 {
-    if (size < 1) {
-        return;             
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << "  ";
     }
-    mergeSort(arr, 0, size - 1, k);    
+    std::cout << "\n";
 }
 
-void mergeSort(int* arr, const int start, const int end, int const k)
-{
-    if (end - start + 1 > k) {
-        //Divide
-        const int middle = (start + end) / 2;
-            
-        // Conquer
-        mergeSort(arr, start, middle, k);
-        mergeSort(arr, middle + 1, end, k);
-            
-        // Combine
-        merge(arr, start, middle, end);                                                                
-    }
-    else {
-        insertionSort(arr, start, end);
-    }
-} 
-
-/* Inputs: arr: [integer], size: integer
- * Pre: size < maxSize
- * Output: arr : [integer]
- * Post: arr[0] <= arr[1] <= ... <= arr[size - 1]
- */
 void insertionSort(int* arr, int const start, int const end)
 {
-    int key;
-    for (int j = start; j <= end; ++j) {
-        key = arr[j];
-        int i = j - 1;
-        
-        while (i >= start && arr[i] > key) {
-            arr[i + 2] = arr[i--];
+    for (int i = start; i <= end; ++i) {
+        const int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j--];
         }
-        arr[i + 1] = key;
+        arr[j + 1] = key;
     }
 }
 
-/* Inputs: arr: [integer], start, middle, end: integer
- * Pre: start, middle, end < maxSize
- *      arr[start] <= arr[start + 1] <= ... <= arr[middle]
- *      arr[middle + 1] <= arr[middle + 2] <= ... <= arr[end]
- * Output: arr : [integer]
- * Post: arr[start] <= arr[start + 1] <= .... <= arr[end]
- */
-void merge(int* arr, const int start, const int middle, const int end)
+void merge(int* arr, int const left, int const mid, int const right) 
 {
-    const int lSize = middle - start + 1;
-    const int rSize = end - middle;
-    
-    int lArr[maxSize + 1];
-    int rArr[maxSize + 1];
-    
+    int const lSize = mid - left + 1;
+    int const rSize = right - mid;
+
+    int* leftArray = new int[lSize + 1];
+    int* rightArray = new int[rSize + 1];
+
     for (int i = 0; i < lSize; ++i) {
-        lArr[i] = arr[start + i];
+        leftArray[i] = arr[left + i];
     }
+   
     for (int i = 0; i < rSize; ++i) {
-        rArr[i] = arr[middle + 1 + i];
+        rightArray[i] = arr[mid + 1 + i];
     }
     
-    /// Merging
-    int li = 0, ri = 0;
-    for (int k = start; k <= end; ++k) {
-        if (lArr[li] < rArr[ri] && li < lSize) {
-            arr[k] = lArr[li++];
+
+    int l_idx = 0, r_idx = 0;
+    for (int k = left; k <= right; ++k) {
+        if (leftArray[l_idx] < rightArray[r_idx] && l_idx < lSize) {
+            arr[k] = leftArray[l_idx++];
         }
-        else if (ri < rSize) {
-            arr[k] = rArr[ri++];
+        else if (r_idx < rSize){
+            arr[k] = rightArray[r_idx++];
         }
-        else if (li < lSize) {
-            arr[k] = lArr[li++];
+        else if (l_idx < lSize) {
+            arr[k] = leftArray[l_idx++];
         }
+    }
+
+    delete[] leftArray;
+    delete[] rightArray;
+}
+
+void mergesort(int* arr, int const left, int const right, const int k) {
+    
+    if (right - left + 1 > k) {
+        const int mid = (left + right) / 2;
+
+        mergesort(arr, left, mid, k);
+        mergesort(arr, mid + 1, right, k);
+
+        merge(arr, left, mid, right);
+    }
+    else {
+        insertionSort(arr, left, right);
     }
 }
 
+// Wraper
+void mergeSort(int* arr, const int size, const int k)
+{
+    mergesort(arr, 0, size - 1, k);
+}
+
+void sort(int* arr, const int size)
+{
+    if (size < 1) {
+        return;
+    }
+
+    const int k = 4;
+    mergeSort(arr, size, k);
+}
+
+int main() {
+    int size;
+    inputSize(size);
+
+    int* arr = new int[size];
+    input(arr, size);
+
+    sort(arr,size);
+    printArr(arr, size);
+}
