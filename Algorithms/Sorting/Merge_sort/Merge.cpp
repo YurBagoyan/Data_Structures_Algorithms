f8a4a952f614a359add103dfd4ae539a32bc3e54
Yura Bagoyan <90376899+YurBagoyan@users.noreply.github.com>
2023-01-16 22:05:52 +0400

    Update Merge.cpp

--- Algorithms/Sorting/Merge_sort/Merge.cpp
+++ Algorithms/Sorting/Merge_sort/Merge.cpp
@@ -30,30 +30,33 @@ void merge(int* arr, int const left, int const mid, int const right)
     int const n1 = mid - left + 1;
     int const n2 = right - mid;
 
-    int* L = new int[n1 + 1];
-    int* R = new int[n2 + 1];
+    int* leftArray = new int[n1 + 1];
+    int* rightArray = new int[n2 + 1];
 
     constexpr int MAX = 1000000;
 
     for (int i = 0; i < n1; ++i) {
-        L[i] = arr[left + i];
+        leftArray[i] = arr[left + i];
     }
-    L[n1] = MAX;
+    leftArray[n1] = MAX;
 
     for (int i = 0; i < n2; ++i) {
-        R[i] = arr[mid + 1 + i];
+        rightArray[i] = arr[mid + 1 + i];
     }
-    R[n2] = MAX;
+    rightArray[n2] = MAX;
 
     int i = 0, j = 0;
     for (int k = left; k <= right; ++k) {
-        if (L[i] < R[j]) {
-            arr[k] = L[i++];
+        if (leftArray[i] < rightArray[j]) {
+            arr[k] = leftArray[i++];
         }
         else {
-            arr[k] = R[j++];
+            arr[k] = rightArray[j++];
         }
     }
+
+    delete[] leftArray;
+    delete[] rightArray;
 }
 
 void mergesort(int* arr, int const left, int const right) {
